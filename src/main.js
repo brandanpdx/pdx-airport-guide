da561ec6343f9d66f0546e5126497cf6e2487fe3
Student <example@example.com>
2020-02-17 14:36:48 -0800

    add back-end logic for google map

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import $ from 'jquery';
+import { Map } from './map-service';
+//import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  let mapElement = document.getElementById('map');
+  
+  Map.loadGoogleMapsApi().then(function(googleMaps) {
+    Map.createMap(googleMaps, mapElement);
+  });
+});
