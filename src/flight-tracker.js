5208edef8afd3e87040d7a3ff5713f73d8b044f7
Student <example@example.com>
2020-02-17 16:47:12 -0800

    made changes to flight-tracker and main.js

--- src/flight-tracker.js
+++ src/flight-tracker.js
@@ -1,10 +1,11 @@
 export class FlightTracker {
-  async getFlight(flightNumber, date) {
+  async getFlight(flightNumber) {
+    console.log(flightNumber)
     try {
-      fetch(`https://aerodatabox.p.rapidapi.com/flights/${flightNumber}/${date}?withLocation=false&withAircraftImage=false`, {
+      let response = await fetch(`https://aerodatabox.p.rapidapi.com/flights/DL47/2019-08-29?withLocation=false&withAircraftImage=false`, {
         "method": "GET",
         "headers": {
-          "x-rapidapi-host": aerodatabox.p.rapidapi.com,
+          "x-rapidapi-host": "aerodatabox.p.rapidapi.com",
           "x-rapidapi-key": `${process.env.API_KEY}`,
           "mode": "no-cors"
         }
@@ -15,6 +16,7 @@ export class FlightTracker {
       } else {
         jsonifiedResponse = false;
       }
+      console.log(jsonifiedResponse);
       return jsonifiedResponse;
     } catch (error) {
       return false;
--- src/main.js
+++ src/main.js
@@ -1,15 +1,46 @@
-import { Map } from './map-service';
+// import { Map } from './map-service.js';
 import $ from 'jquery';
 $
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { FlightTracker } from './../src/flight-tracker';
 
 
-document.addEventListener("DOMContentLoaded", function() {
-  let mapElement = document.getElementById('map');
+// document.addEventListener("DOMContentLoaded", function() {
+//   let mapElement = document.getElementById('map');
   
-  Map.loadGoogleMapsApi().then(function(googleMaps) {
-    Map.createMap(googleMaps, mapElement);
-  });
-});
+//   Map.loadGoogleMapsApi().then(function(googleMaps) {
+//     Map.createMap(googleMaps, mapElement);
+//   });
+// });
+
+$(document).ready(function() {
+  $("#flightInformation").click(function(event) {
+    event.preventDefault();
+
+    let tracker = new FlightTracker();
+      const flightNumber = $('#flight').val();
+      const date = $('#date').val()
+      console.log(flightNumber);
+      console.log(date)
+      console.log(tracker);
+    (async () => {
+      let response = await tracker.getFlight(flightNumber); 
+      getElements(response);
+      function getElements(response) {
+        if (response === false) {
+          alert("there was an error in processing you request");
+        }
+        else if (response.meta.total === 0) {
+          alert("we apologize, no results match your search");
+        }
+        else {
+          $(".showairline").text("test");
+          
+          
+        }
+      }
+    })
+  })
+})
