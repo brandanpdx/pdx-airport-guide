0ea5a70db17b415f0c3c0098d3e118ea3ce8744c
Student <example@example.com>
2020-02-18 13:43:40 -0800

    add price_level to restaurant info function

--- src/airport-info-details.js
+++ src/airport-info-details.js
@@ -1,7 +1,27 @@
 export function restaurantInfo (results) {
   let restaurant = {
-    name:`${results.name}`
+    name:`${results.name}`,
+    image:`${results.icon}`
   };
-  let restaurantDetails = `<div class="col-sm-4">${restaurant.name}</div>`;
+  
+  let openNow =`${results.opening_hours.open_now}`;
+    if( openNow !== "true"){
+      openNow = "close"
+    } else {
+      openNow = "open"
+    }
+
+  let price = `${results.price_level}`;
+    if(price == null) {
+      price = "x"
+    } else {
+      price;
+    }
+ 
+  let restaurantDetails = 
+  `<div class="col-sm-4">${restaurant.name}<br>
+   <img src='${restaurant.image}'<br>
+   Open now: ${openNow}<br>
+   Price level: ${price}</div>`;
   return restaurantDetails;
 }
