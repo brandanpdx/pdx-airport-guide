e992dd25f9d634b5eea34f7ba3e0eadc625e4bad
Student <example@example.com>
2020-02-18 09:53:44 -0800

    create restaurant-info.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { Map } from './map-service.js';
-import $ from 'jquery';
+// import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { FlightTracker } from './../src/flight-tracker';
+// import { FlightTracker } from './../src/flight-tracker';
 
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -12,48 +12,47 @@ document.addEventListener("DOMContentLoaded", function() {
   Map.loadGoogleMapsApi().then(function(googleMaps) {
     Map.createMap(googleMaps, mapElement);
   });
-
-});
-
-$(document).ready(function() {
-  $("#flightInformation").click(function(event) {
-    event.preventDefault();
-
-    let tracker = new FlightTracker();
-      const flightNumber = $('#flight').val();
-      const date = $('#date').val()
-      console.log(flightNumber);
-      console.log(date);
-      console.log(tracker);
-    (async () => {
-      let response = await tracker.getFlight(flightNumber); 
-      getElements(response);
-      function getElements(response) {
-        if (response === false) {
-          alert("there was an error in processing you request");
-        }
-        else if (response.meta.total === 0) {
-          alert("we apologize, no results match your search");
-        }
-        else {
-          $(".showairline").text("test");
-          
-          
-        }
-      }
-    })
-  })
-})
-
-  
   (async () => {
-    let map = new Map();
-    const response = await map.getPlace();
-    getElements(response);
-  }) ();
+      let map = new Map();
+      const response = await map.getPlace();
+      getElements(response);
+    }) ();
 
-  function getElements(response) {
-    console.log(response);
-  }
-
+    function getElements(response) {
+      console.log(response);
+    }
 });
+
+
+
+
+
+
+// $(document).ready(function() {
+//   $("#flightInformation").click(function(event) {
+//     event.preventDefault();
+
+//     let tracker = new FlightTracker();
+//       const flightNumber = $('#flight').val();
+//       const date = $('#date').val()
+//       console.log(flightNumber);
+//       console.log(date);
+//       console.log(tracker);
+//     (async () => {
+//       let response = await tracker.getFlight(flightNumber); 
+//       getElements(response);
+//       function getElements(response) {
+//         if (response === false) {
+//           alert("there was an error in processing you request");
+//         }
+//         else if (response.meta.total === 0) {
+//           alert("we apologize, no results match your search");
+//         }
+//         else {
+//           $(".showairline").text("test");
+//         }
+//       }
+//     })
+//   })
+
+ 
